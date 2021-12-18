deeb443a904bbd69361eec88c356b2bae36c208b
Addy Rodriguez <rdz.33.a@gmail.com>
2021-12-18 15:36:16 -0600

    Iron Man's abilities loop

--- the_avengers.js
+++ the_avengers.js
@@ -27,9 +27,13 @@ const ironMan = {
     }
 }
 
-// 1. Log Iron Man's third ability
-console.log(ironMan.abilities[2]);
+// // 1. Log Iron Man's third ability
+// console.log(ironMan.abilities[2]);
+
 // 1. Log all of Iron Man's abilities by looping through the array
+for(let i =0; i < ironMan.abilities.length;i++){
+    console.log(ironMan.abilities[i]);
+}
 // 1. Log whether or not Iron Man appeared in `spiderManHomecoming`
 // 1. Wait, that's not right -- he did appear in that movie! Change the value to true without editing the object itself and log it again to verify it changed
 // 1. Check if jarvis is there by calling on the `jarvisAreYouThere` function 
